d967e3a09c7f1c1e83779ef3c103f826951ad9f6
linuxlover <linuxlover@gmail.com>
2017-02-20 14:43:45 +0100

    New version

--- adm_modeltester/__init__.py
+++ adm_modeltester/__init__.py
@@ -1,5 +1,5 @@
 __title__ = 'Django ADM Model Tester'
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 __author__ = 'Alfredo Di Maria'
 __license__ = 'GPL 3'
 __copyright__ = 'Copyright 2017 ADM Automation'
